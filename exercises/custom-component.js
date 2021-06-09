714292ffe04e9991bfa4bd8a488c229b2ca6c589
Hui <xuhuizhu369@gmail.com>
2021-06-09 08:05:28 -0700

    renders custom configurable button component

--- exercises/custom-component.js
+++ exercises/custom-component.js
@@ -1,6 +1,59 @@
 // Creating a Custom Component
+import React from 'react';
+import { TouchableOpacity, Text, Alert } from 'react-native';
 
 export default () => {
-  // return <Button text="Hello" onPress={() => alert('hello!')} />
-  return null;
+
+  const sayHello = () => {
+    Alert.alert('Hello Alert', 'hello!');
+  }
+
+  const sayGoodbye = () => {
+    Alert.alert('Goodbye Alert', 'goodbye!');
+  }
+
+  return (
+    <>
+      <CustomButton
+        text='Say hello'
+        onPress={sayHello}
+        buttonStyle={helloStyles}
+      />
+      <CustomButton
+        text='Say goodbye'
+        onPress={sayGoodbye}
+        buttonStyle={goodbyeStyles}
+      />
+    </>
+  )
+
 };
+
+const CustomButton = ({ onPress, text, buttonStyle }) => {
+
+  return (
+    <TouchableOpacity 
+      onPress={onPress}
+      style={{...buttonStyle, ...buttonStyles}}
+    >
+      <Text>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const buttonStyles = {
+  alignSelf: 'center',
+  width: 150,
+  height: 75,
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: 25
+}
+
+const helloStyles = {
+  backgroundColor: 'pink',
+}
+
+const goodbyeStyles = {
+  backgroundColor: 'turquoise',
+}
