f53e8431cae6ca867e8ac919b93c890cfa767f21
Hui <xuhuizhu369@gmail.com>
2021-06-09 08:59:19 -0700

    centers three boxes and configures their text/styling with props

--- exercises/styling.js
+++ exercises/styling.js
@@ -1,1 +1,42 @@
 // Styling
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default () => {
+
+    return (
+      <>
+        <View style={{ 
+            flex: 1,
+            flexDirection: 'row', 
+            justifyContent: 'center', 
+            alignItems: 'center' 
+        }}
+        >
+          <Square text="Square 1" color='lightblue' />
+          <Square text="Square 2" color='teal' />
+          <Square text="Square 3" color='salmon' />
+        </View>
+      </>
+    )
+};
+
+const Square = ({ text, color }) => {
+    return (
+      <>
+        <View style={{ backgroundColor: color, ...SquareStyles.box }}>
+          <Text>{text}</Text>
+        </View>
+      </>
+    );
+}
+
+const SquareStyles = StyleSheet.create({
+    box: {
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 20
+    }
+})
