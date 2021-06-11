e7426a847586ffe8b12d345226e0f098f033c456
Hui <xuhuizhu369@gmail.com>
2021-06-11 06:18:18 -0700

    implements listener to update count on interval and clear itself on unmount

--- exercises/component-lifecycle.js
+++ exercises/component-lifecycle.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 
 import { listener, listener2 } from "../support/component-lifecycle";
 
 export default () => {
   const [updateCount, setUpdateCount] = useState(0);
+
+  useEffect(() => {
+    const handleUpdatingCount = () => {
+      setUpdateCount(c => c+1);
+    }
+
+    const listenItvl = listener(handleUpdatingCount);
+
+    return () => listenItvl();
+  }, []);
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
