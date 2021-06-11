3005869f5d3b1aecd214076c21d395f2486ece96
Hui <xuhuizhu369@gmail.com>
2021-06-11 04:47:54 -0700

    init a class compo

---
+++ exercises/class-components.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class ClassComponent extends React.Component {
+
+    render() {
+        return (
+          <View>
+            <Text>hello gaiz</Text>
+          </View>
+        )
+    }
+}
+
+export default ClassComponent;
